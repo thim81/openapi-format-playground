ab30b3472991cb23583cf5a74d373d70bd821be9
Thim <timhaselaars@gmail.com>
2024-06-06 23:58:27 +0200

    Share option (#3)
    * Added share option
    
    * release 0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+## unreleased
+
+## [0.1.1] - 2024-06-06
+
+- Added option to share playground with input, output and config
+- Improved error handling
+
+
+## [0.1.0] - 2024-06-05
+
+Initial release
+
---
+++ src/components/ShareButton.tsx
@@ -0,0 +1,44 @@
+// components/ShareButton.tsx
+import React from 'react';
+import {Base64} from "js-base64";
+import { gzip } from 'pako'
+
+interface ShareButtonProps {
+  data: string;
+  config: {
+    sort: boolean;
+    filterOptions: string;
+    sortOptions: string;
+    isFilterOptionsCollapsed: boolean;
+    isSortOptionsCollapsed: boolean;
+    outputLanguage: 'json' | 'yaml';
+  };
+}
+
+const ShareButton: React.FC<ShareButtonProps> = ({data, config }) => {
+  const handleShare = async () => {
+    // Encode
+    const url = new URL(globalThis.location.href)
+    const encodedInput = Base64.fromUint8Array(gzip(data))
+    const encodedConfig = Base64.fromUint8Array(gzip(JSON.stringify(config || {})))
+
+    url.searchParams.set('config', encodedConfig)
+    url.searchParams.set('input', encodedInput)
+
+    const shareUrl = url.toString()
+    console.log('shareUrl', shareUrl)
+
+    if (typeof window !== 'undefined') {
+      window.history.replaceState(null, '', shareUrl)
+      await navigator.clipboard.writeText(shareUrl)
+    }
+  }
+
+  return (
+    <button onClick={handleShare} className="bg-green-500 hover:bg-green-700 text-white font-medium text-sm py-1 px-4 rounded">
+      Share
+    </button>
+  );
+};
+
+export default ShareButton;
